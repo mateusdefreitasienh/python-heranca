16dd195be3c184b21104930fa65812611d82739e
Mateus de Freitas <mateusdefreitas@ienh.com.br>
2026-06-09 21:54:56 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from veiculo import Veiculo
+
+moto = Veiculo(marca = "Yamaha", 
+               modelo="MT-30", 
+               cor="Preta", 
+               ano=2025, 
+               placa="ABC-1E34")
+
+
+print(moto)
